9aab993c5093d9ee6fbce3254c78f35328359690
Guillermo Barrientos <barrientos.memo@gmail.com>
2017-09-10 21:46:10 -0500

    Primera entrega

---
+++ ionic/src/pages/index/index.ts
@@ -0,0 +1,91 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { AlertController } from 'ionic-angular';
+import {HomePage} from '../home/home';
+
+const TICKETS: Ticket[] = [
+  {id: 46263,
+  cliente: 'Gerardo Aguirre',
+  fechaAper: '2017-09-01',
+  fechaCierre: '2017-09-02',
+  numNovedades: 3,
+  estado: 'cerrado',
+  descripcion: 'Falla carga de portal'},
+
+  {id: 46264,
+  cliente: 'Edgar Toledo',
+  fechaAper: '2017-09-02',
+  fechaCierre: '2017-09-02',
+  numNovedades: 1,
+  estado: 'cerrado',
+  descripcion: 'Instalar la nueva version'},
+
+  {id: 46265,
+  cliente: 'Fernando Ruiz',
+  fechaAper: '2017-09-03',
+  fechaCierre: '2017-09-04',
+  numNovedades: 4,
+  estado: 'cerrado',
+  descripcion: 'Activar nueva cuenta'},
+
+  {id: 46266,
+  cliente: 'Alfredo Puente',
+  fechaAper: '2017-09-08',
+  fechaCierre: '2017-09-12',
+  numNovedades: 2,
+  estado: 'pendiente',
+  descripcion: 'No carga el portal'}
+]
+
+@IonicPage()
+@Component({
+  selector: 'page-index',
+  templateUrl: 'index.html',
+})
+export class IndexPage {
+  homePage = HomePage;
+  tickets =  TICKETS;
+  
+  constructor(public navCtrl: NavController, public navParams: NavParams, public alertCtrl: AlertController) {
+  }
+
+  doPrompt() {
+    let prompt = this.alertCtrl.create({
+      title: 'Nuevo Ticket',
+      message: "Ingrese los datos del Ticket",
+      inputs: [
+        {name: 'titulo', placeholder: "Título"},
+        {name: 'descripción', placeholder: "Descripción"},
+        {name: 'prioridad', placeholder: "Prioridad"},
+      ],
+      buttons: [
+        {
+          text: 'Cancelar',
+          handler: data => {
+            console.log('Cancel clicked');
+          }
+        },
+        {
+          text: 'Guardar',
+          handler: data => {
+            console.log('Saved clicked');
+          }
+        }
+      ]
+    });
+    prompt.present();
+  }
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad IndexPage');
+  }
+
+}
+export class Ticket{
+  id: number;
+  cliente: string;
+  fechaAper: string;
+  fechaCierre: string;
+  numNovedades: number;
+  estado: string;
+  descripcion: string;
+}
